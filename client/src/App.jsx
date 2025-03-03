32065d1cf8012fe4577844d3d2f3961044b95cb6
Kaustubh <kaustubhthakur76@gmail.com>
2025-03-03 22:08:24 +0530

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import {Route ,Routes} from "react-router-dom"
+import HomePage from './pages/homepage/HomePage'
+import RegisterForm from './pages/registerpage/RegisterForm'
+import LoginForm from './pages/login/LoginForm'
 const App = () => {
   return (
-    <div>App</div>
+    <Routes>
+      <Route path='/' element={<HomePage/>} />
+      <Route path='/register' element={<RegisterForm/>} />
+      <Route path='/login' element={<LoginForm/>} />
+    </Routes>
   )
 }
 
---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Menu, X } from "lucide-react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <div className="logo">LOGO</div>
+        
+        <div className="hamburger" onClick={() => setIsOpen(!isOpen)}>
+          {isOpen ? <X size={28} /> : <Menu size={28} />}
+        </div>
+        
+        <div className={`menu ${isOpen ? "open" : ""}`}>
+          <Link to="/register" className="nav-link">Register</Link>
+          <Link to="/login" className="nav-link">Login</Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
