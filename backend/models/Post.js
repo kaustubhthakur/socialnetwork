4017478112f6138aff9c60af4d9c7426b9c26dbd
Kaustubh <kaustubhthakur76@gmail.com>
2025-03-23 23:02:41 +0530

    first commit

---
+++ backend/models/Post.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const PostSchema = new mongoose.Schema({
+    desciption:{
+        type:String,
+        required:true,
+    },
+    image:{
+        type:String,
+        required:true,
+    },
+    userId:{
+        type:String,
+        required:true,
+    },
+    votes:{
+        type:[String],
+        default:[],
+    }
+
+},{
+    timestamps:true,
+})
+module.exports = mongoose.model("Post",PostSchema);
---
+++ backend/models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+const UserSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    posts:{
+        type:[String],
+        default:[],
+    },
+    profilepic:{
+        type:String,
+        default:" ",
+    },
+    description:{
+        type:String,
+        default:"",
+        }
+},{
+    timestamps:true,
+})
+module.exports = mongoose.model("User",UserSchema);
