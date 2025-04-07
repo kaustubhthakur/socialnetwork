3841d4f81d040c217667fb5591d8ff831f70ad2b
Kaustubh <kaustubhthakur76@gmail.com>
2025-04-08 00:57:29 +0530

    first commit

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -19,7 +19,6 @@ const Navbar = () => {
               <svg fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 10V3L4 14h7v7l9-11h-7z" />
               </svg>
-              MyLogo
             </Link>
           </div>
 
